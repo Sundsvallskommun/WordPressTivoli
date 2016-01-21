4faabb58b77383155df0d117480a920e830d808c
Johan Linder <johan@flatmate.se>
2016-01-21 11:52:04 +0100

    Add google translate dropdown to header

---
+++ sundsvall_se-theme/partials/google-translate.php
@@ -0,0 +1,21 @@
+<div class="nav-item dropdown translation-dropdown">
+
+	<button class="btn btn-info nav-link dropdown-toggle" lang="en" type="button" id="languageMenuButton" aria-haspopup="true" aria-expanded="false">
+		Language
+	</button>
+	<div class="dropdown-menu" aria-labelledby="languageMenuButton">
+
+		<h6 class="dropdown-header" lang="en">Translate</h6>
+		<p class="dropdown-item" lang="en" style="white-space: normal;"><small>Use Google to translate the web site. We take no responsibility for the accuracy of the translation.</small></p>
+
+		<div class="dropdown-item" id="google_translate_element"></div><script type="text/javascript">
+		function googleTranslateElementInit() {
+			new google.translate.TranslateElement({pageLanguage: 'sv', autoDisplay: false, multilanguagePage: false}, 'google_translate_element');
+		}
+		</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+	</div>
+
+</div>
+
+
--- sundsvall_se-theme/partials/site-navbar.php
+++ sundsvall_se-theme/partials/site-navbar.php
@@ -1,8 +1,13 @@
 <nav role="navigation" class="navbar navbar-full bg-faded">
 	<div class="container-fluid">
 		<div class="nav navbar-nav pull-xs-right">
+
 			<a class="nav-item nav-link btn btn-secondary" href="https://e-tjanster.sundsvall.se/">Våra e-tjänster</a>
+
+			<?php get_template_part('./partials/google-translate'); ?>
+
 			<a class="nav-item nav-link btn btn-primary" href="#">Lyssna</a>
+
 		</div>
 	</div>
 </nav>
