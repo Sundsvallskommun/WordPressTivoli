da9d5760f8c171eef358646f9ee11a2d4f629821
Johan Linder <johan@flatmate.se>
2016-02-01 13:53:20 +0100

    Improve js (theme + bootstrap) build in gulpfile

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ WordPress-plugins:
 2. Installera DB
 3. Lägg in eller symlinka temat i `wp-content/themes`
 4. Installera och aktivera WordPress-plugins (se beroenden ovan).
+
+# Utveckling
+
+## JavaScript
+
+All JS i temat körs via Babel (med ES2015), läggs samman och minifieras. De
+filer som ska ingå anges i `gulpfile.js` i `themeScripts`-variableln.
+
+Det är angivet att alla filer som ligger direkt under `assets/js/source/`
+följer med.  Om filer i undermappar ska användas läggs dessa till i arrayen.
+
+Alla bootstrap-komponenters script finns angivna, men de som inte används är
+utkommenterade.
--- sundsvall_se-theme/gulpfile.js
+++ sundsvall_se-theme/gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp         = require('gulp'),
     watch        = require('gulp-watch'),
+    babel        = require('gulp-babel'),
     sass         = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     sourcemaps   = require('gulp-sourcemaps'),
     concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
     browserSync  = require('browser-sync').create();
 
 var config = {
@@ -17,28 +19,51 @@ gulp.task('styles', function() {
 	gulp.src('./assets/css/scss/style.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.write())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./assets/css'))
 		.pipe(browserSync.stream());
 });
 
 gulp.task('editor-styles', function() {
 	gulp.src('./assets/css/scss/editor-styles.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./assets/css'));
 });
 
+var themeScripts = [
+	'*.js',
+	//'/bootstrap/util.js',
+	//'/bootstrap/alert.js',
+	//'/bootstrap/button.js',
+	//'/bootstrap/carousel.js',
+	//'/bootstrap/collapse.js',
+	'/bootstrap/dropdown.js',
+	//'/bootstrap/modal.js',
+	//'/bootstrap/scrollspy.js',
+	//'/bootstrap/tab.js',
+	//'/bootstrap/tooltip.js',
+	//'/bootstrap/popover.js'
+].map(s => './assets/js/source/' + s);
+
 gulp.task('scripts', function() {
-	gulp.src('./assets/js/source/**/*.js')
+	gulp.src(themeScripts)
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['es2015']
+		}))
 		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./assets/js'))
 		.pipe(browserSync.stream());
 });
@@ -50,15 +75,17 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', ['default'], function() {
-	watch('./assets/css/scss/**/*.scss', function() {
+
+	var watchArgs = { interval: 500 };
+	watch('./assets/css/scss/**/*.scss', watchArgs,function() {
 		gulp.start('styles');
 	});
 
-	watch('./assets/js/source/**/*.js', function() {
+	watch('./assets/js/source/**/*.js', watchArgs, function() {
 		gulp.start('scripts');
 	});
 
-	watch('./**/*.php', function() {
+	watch('./**/*.php', watchArgs,function() {
 		browserSync.reload();
 	});
 });
