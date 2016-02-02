e9f3351af989592252a17ae380e3b3a2f9545d16
Johan Linder <johan@flatmate.se>
2016-02-02 14:50:08 +0100

    Add temporary icon to top level menu items

--- sundsvall_se-theme/partials/site-navigation.php
+++ sundsvall_se-theme/partials/site-navigation.php
@@ -4,8 +4,29 @@
 		'theme_location'  => 'main-menu',
 		'container'       => false,
 		'menu_class'      => 'container-fluid list-inline',
-		'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+		'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'walker'          => new Menu_Icons_Walker()
 	);
 	wp_nav_menu( $nav_args );
 ?>
 </nav>
+
+<?php
+	class Menu_Icons_Walker extends Walker_Nav_Menu {
+
+		function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+			$output .= sprintf( "\n<li>
+				<a href='%s'%s>
+					<span class='menu-item-icon'>%s</span>
+					<span class='menu-item-text'>%s</span>
+				</a>
+				</li>\n",
+				$item->url,
+				( $item->object_id === get_the_ID() ) ? ' class="current"' : '',
+				get_icon('bo-miljo'),
+				$item->title
+			);
+		}
+
+	}
+
