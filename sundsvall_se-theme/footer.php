0186f22b9019382e3077b81902838b7c58012ecc
Johan Linder <johan@flatmate.se>
2016-02-11 10:02:45 +0100

    Style two columns in footer with static data

--- sundsvall_se-theme/footer.php
+++ sundsvall_se-theme/footer.php
@@ -6,19 +6,67 @@
 
 		<div class="row">
 
-			<div class="col-md-4">
+			<div class="col-md-4 site-footer__contact-block">
 
-				<img src="#" alt="Sundsvalls Kommun">
+				<div class="site-logo">
+					<img src="<?php bloginfo('template_directory')?>/assets/images/logo-white.svg" alt="<?php printf(__('%s logotyp, länk till startsidan.', 'sundsvall_se'), bloginfo('title')); ?>">
+				</div>
+
+				<div class="contact-item contact-item--address">
+					<span class="footer-icon">
+						<?php the_icon('home', array('alt' => 'Adress')); ?>
+					</span>
+					<p>
+						Sundsvalls Kommun <br>
+						Norrmalmsgatan 4, 851 85 Sundsvall
+					</p>
+				</div>
+
+				<div class="contact-item contact-item--email">
+					<span class="footer-icon">
+						<?php the_icon('message', array('alt' => 'E-post')); ?>
+					</span>
+					<p>
+						<a href="mailto:sundsvalls.kommun@sundsvall.se">sundsvalls.kommun@sundsvall.se</a>
+					</p>
+				</div>
+
+				<div class="contact-item contact-item--phone">
+					<span class="footer-icon">
+						<?php the_icon('telephone', array('alt' => 'Telefon')); ?>
+					</span>
+					<p>
+						<a href="">060-19 10 00</a>
+					</p>
+				</div>
+
+				<p class="extra-contacts">Kontaktuppgifter till alla anställda hittar du här</p>
 
 			</div>
 
-			<div class="col-md-4">
+			<div class="col-md-4 site-footer__about-block">
 
 				<h2>Om sundsvall.se</h2>
 
+				<p>Sundsvall.se är Sundsvalls kommuns offentliga webbplats.</p>
+
+				<nav>
+					<ul class="list-unstyled">
+						<li><a href="#">Om webbplatsen</a></li>
+						<li><a href="#">Om cookies</a></li>
+						<li><a href="#">Press- och informationsmaterial</a></li>
+						<li><a href="#">Tyck till om webbplatsen</a></li>
+						<li><a href="#">Våra RSS-flöden</a></li>
+					</ul>
+				</nav>
+
 			</div>
 
-			<div class="col-md-4"></div>
+			<div class="col-md-4">
+
+				<p>Har du frågor om webbplatsen?</p>
+
+			</div>
 
 		</div>
 
