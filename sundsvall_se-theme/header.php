6126f85f875920f59a3187969502d87ce6025311
Johan Linder <johan@flatmate.se>
2016-02-12 16:32:25 +0100

    Minor improvements to mobile navbar

--- sundsvall_se-theme/header.php
+++ sundsvall_se-theme/header.php
@@ -1,10 +1,14 @@
 <?php get_template_part('head'); ?>
 
 <nav class="hidden-md-up navbar navbar-light navbar-full navbar-mobile navbar-sticky-top">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavigation">
+		&#9776;
+	</button>
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavigation">
-    &#9776;
-  </button>
+	<div class="navbar-center">
+			<?php get_template_part('partials/site-logo'); ?>
+	</div>
+
 
 		<button type="button" class="navbar-toggler pull-xs-right btn"
 						data-toggle="collapse" data-target="#searchContainer"><?php the_icon('search'); ?></button>
