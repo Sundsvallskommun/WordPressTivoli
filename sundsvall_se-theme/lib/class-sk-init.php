1556d1e210bd1b61bbab0b453c8bc7d25d84ac4e
Johan Linder <johan@flatmate.se>
2016-02-16 09:34:58 +0100

    Set what TinyMCE toolbar items should be visible

--- sundsvall_se-theme/lib/class-sk-init.php
+++ sundsvall_se-theme/lib/class-sk-init.php
@@ -15,6 +15,8 @@ class SK_Init {
 
 		add_action('admin_head', array(&$this, 'template_dir_js_var'));
 
+		add_filter('tiny_mce_before_init', array(&$this, 'tiny_mce_settings') );
+
 	}
 
 	function template_dir_js_var() {
@@ -24,5 +26,39 @@ class SK_Init {
 		</script>
 		<?php
 	}
+
+	/**
+	 * TinyMCE-settings
+	 *
+	 * @author Johan Linder <johan@flatmate.se>
+	 */
+	function tiny_mce_settings($settings) {
+
+		/**
+		 * Select what to be shown in tinymce toolbars.
+		 *
+		 * Original settings:
+		 *
+		 * toolbar1 = 'bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,fullscreen,wp_adv,eservice_button'
+		 * toolbar2 = 'formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help'
+		 */
+		$settings['toolbar1'] = 'fullscreen, formatselect, bold, bullist, numlist, blockquote, link, unlink, spellchecker, eservice_button, wp_adv';
+		$settings['toolbar2'] = 'pastetext, removeformat, charmap, outdent, indent, undo, redo, wp_help';
+
+		/**
+		 * Always show toolbar 2
+		 */
+		//$settings['wordpress_adv_hidden'] = false;
+
+		/**
+		 * Block formats to show in editor dropdown. We remove h1 as we set page
+		 * title to h1 in the theme.
+		 */
+		$settings['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;';
+
+		return $settings;
+
+	}
+
 }
 
