5155f9387982ac272ddc374f8da4ec9cb12a1d75
Johan Linder <johan@flatmate.se>
2016-02-18 11:40:25 +0100

    Eservice-category page widget

--- sundsvall_se-theme/lib/sk-eservices/class-sk-eservices.php
+++ sundsvall_se-theme/lib/sk-eservices/class-sk-eservices.php
@@ -11,6 +11,8 @@ class SK_EServices {
 		add_action('init', array(&$this, 'eservice_shortcode_button_init'));
 
 		add_action('wp_ajax_eservice', array(&$this, 'ajax_eservice'));
+
+		add_action('sk_page_widgets', array(&$this, 'widget_eservice_category'));
 	}
 
 	/**
@@ -79,7 +81,7 @@ class SK_EServices {
 
 		if(!$service) {
 
-			$widget  = '<div class="eservice-single eservice-single--notfound">';
+			$widget  = '<div class="eservice-single-block eservice-single-block--notfound">';
 			$widget .= '<span>';
 			$widget .= 'Hoppsan, det gick inte att hitta e-tjänsten. Gå till <a href="https://e-tjanster.sundsvall.se">E-tjänsteportalen</a> eller titta tillbaka senare.';
 			$widget .= '</span>';
@@ -97,7 +99,7 @@ class SK_EServices {
 
 
 
-		$widget  = '<a href="'.$service_url.'" class="eservice-single">';
+		$widget  = '<a href="'.$service_url.'" class="eservice-single-block">';
 		$widget .= '<img src="'.$service_icon.'">';
 		$widget .= '<span>';
 		$widget .= $service_name;
@@ -122,6 +124,73 @@ class SK_EServices {
 		}
 
 		wp_die();
+	}
+
+	/**
+	 * Page widget listing all services in category if specified in page settings.
+	 *
+	 * @author Johan Linder <johan@flatmate.se>
+	 */
+	function widget_eservice_category() {
+
+		global $post;
+
+		$cat = get_field('eservices_category', $post->ID);
+
+		if(!isset($cat)) return;
+
+		$eservices = $this->oep->get_category($cat);
+
+		if(empty($eservices)) return;
+
+		$markup = apply_filters('sk_page_widget_markup', '
+			<div class="page-widget widget-eservices">
+				<div class="page-widget__container">
+					<div class="row">
+						<div class="col-xs-12">
+							<h3 class="page-widget__title">%s</h3>
+							%s
+						</div>
+					</div>
+				</div>
+			</div>');
+
+		$title = __('Alla etjänster för ', 'sundsvall_se').'<strong>'.$eservices[0]['Category'].'</strong>';
+
+		$eservice_links = '<div class="page-widget__columns"><ul>';
+		$eservice_links .= $this->eservice_links($eservices, '<li>%s</li>');
+		$eservice_links .= '</ul></div>';
+
+		printf($markup, $title, $eservice_links);
+
+	}
+
+	/**
+	 * Generate eservice-links from array of eservices.
+	 *
+	 * @author Johan Linder <johan@flatmate.se>
+	 *
+	 * @param array $eservices
+	 */
+	function eservice_links($eservices, $linkwrap) {
+
+		$links = '';
+
+		foreach($eservices as $eservice) {
+			$name = $eservice['Name'];
+			$url  = $eservice['URL'];
+
+			$link = apply_filters('sk_eservice_link', sprintf('<a class="eservice-link" href="%s">%s</a>', $url, $name), $url, $name);
+
+			if(isset($linkwrap)) {
+				$links .= sprintf($linkwrap, $link);
+			} else {
+				$links .= $link;
+			}
+		}
+
+		return $links;
+
 	}
 
 }
