91526c4e85086106a85f3886c538950acc0afd42
Johan Linder <johan@flatmate.se>
2016-02-23 09:51:49 +0100

    Add comments describing what each include does in functions.php

--- sundsvall_se-theme/functions.php
+++ sundsvall_se-theme/functions.php
@@ -1,50 +1,152 @@
 <?php
-function register_sk_menus() {
-  register_nav_menus(
-    array(
-      'main-menu' => __( 'Huvudmeny' )
-    )
-  );
-}
-add_action( 'init', 'register_sk_menus' );
-
+/**
+ * SK_Init
+ * =======
+ *
+ * Post-thumbnails
+ *
+ * Register menus
+ *
+ * TinyMCE settings
+ *
+ * Body section class
+ *   Sets a class to body depending on what menu structure you are visiting.
+ */
 require_once locate_template( 'lib/class-sk-init.php' );
 $sk_init = new SK_Init();
 
+/**
+ * sk_log
+ * ======
+ *
+ * Helper-function for logging to SK_LOG_PATH or template_directory/logs/
+ */
 require_once locate_template( 'lib/helpers/sk-logger.php' );
 
+/**
+ * Misc helper functions
+ * =====================
+ *
+ * the_icon, get_icon
+ *   Echo or return svg <use> with specified icon
+ *
+ * format_phone, get_phone_link
+ *   Formats a phone number. Returns phone number anchor.
+ *
+ * get_section_class_name
+ *   Return classname of current menu section. Also see class-sk-init.php
+ *
+ * ancestor_field
+ *   Find closest ancestor that match supplied values.
+ */
 require_once locate_template( 'lib/helpers/sk-general.php' );
 
-include_once 'lib/breadcrumbs.php';
+/**
+ * Breadcrumbs
+ * ===========
+ *
+ * Function related to outputting breadcrumbs.
+ *
+ * the_breadcrumbs
+ *   echo or return breadcrumbs markup.
+ */
+require_once locate_template('lib/breadcrumbs.php');
 
+/**
+ * SK_Rename_Default_Post_Type
+ * ===========================
+ *
+ * Functions related to shortcut page template
+ */
+require_once locate_template( 'lib/sk-shortcut.php' );
+
+/**
+ * SK_Enqueues
+ * ===========
+ *
+ * Enqueues theme stylesheets and scripts.
+ */
 require_once locate_template( 'lib/class-sk-enqueues.php' );
 $sk_enqueues = new SK_Enqueues();
 
+/**
+ * SK_Helpmenu
+ * ===========
+ *
+ * Functions related to page sidebar helpmenu.
+ */
 require_once locate_template( 'lib/class-sk-helpmenu.php' );
 $sk_helpmenu = new SK_Helpmenu();
 
+/**
+ * SK_Page_Lead
+ * ============
+ *
+ * Automatic .lead-class of first paragraph in theme and editor.
+ */
 require_once locate_template( 'lib/sk-page-lead/class-sk-page-lead.php' );
 $first_paragraph_lead = new SK_Page_Lead();
 
+/**
+ * SK_Tags
+ * =======
+ *
+ * Add tag support for pages
+ */
 require_once locate_template( 'lib/class-sk-tags.php' );
 $sk_tags = new SK_Tags();
 
+/**
+ * SK_Comments
+ * ===========
+ *
+ * Disable comments support
+ */
 require_once locate_template( 'lib/class-sk-comments.php' );
 $sk_comments = new SK_Comments();
 
+/**
+ * SK_Widgets
+ * ==========
+ *
+ *
+ */
 require_once locate_template( 'lib/sk-widgets/class-sk-widgets.php' );
 $sk_widgets = new SK_Widgets();
 
+/**
+ * SK_EServices
+ * ============
+ *
+ * E-service shortcode and page widget.
+ */
 require_once locate_template( 'lib/sk-eservices/class-sk-eservices.php' );
 $sk_eservices = new SK_EServices();
 
+/**
+ * SK_Page_Contacts
+ * ================
+ *
+ * Contact persons post type and display functions.
+ */
 require_once locate_template( 'lib/class-sk-page-contacts.php' );
 $sk_post_type_contacts = new SK_Page_Contacts();
 
+/**
+ * SK_Easyread
+ * ===========
+ *
+ * Add link to sidebar of posts/pages to toggle easylu readable content.
+ */
 require_once locate_template( 'lib/class-sk-easyread.php' );
 $sk_easyread = new SK_Easyread();
 
+/**
+ * SK_Rename_Default_Post_Type
+ * ===========================
+ *
+ * Rename default post-type to "Nyheter"
+ */
 require_once locate_template( 'lib/class-sk-rename-default-post-type.php' );
 $sk_rename_default_post_type = new SK_Rename_Default_Post_Type();
 
-require_once locate_template( 'lib/sk-shortcut.php' );
