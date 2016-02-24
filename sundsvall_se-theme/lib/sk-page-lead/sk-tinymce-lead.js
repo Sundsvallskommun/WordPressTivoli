82888ecf465ea49bca919665d1a3e23b3dac862d
Johan Linder <johan@flatmate.se>
2016-02-24 16:48:35 +0100

    TinyMCE lead-plugin improvement
    * Listen to change in addition to keyup
    * Style lead white on red background if character count is over 200

--- sundsvall_se-theme/lib/sk-page-lead/sk-tinymce-lead.js
+++ sundsvall_se-theme/lib/sk-page-lead/sk-tinymce-lead.js
@@ -1,18 +1,40 @@
 /**
- * 
+ * Add .lead class to first paragraph in editor. Add red styling if longer than
+ * 200 characters
  */
 
 tinymce.PluginManager.add('sk_page_lead', function(editor, url) {
 
 	editor.on('init', function() {
-		tinyMCE.activeEditor.dom.removeClass(tinyMCE.activeEditor.dom.select('p'), 'lead');
-		tinyMCE.activeEditor.dom.addClass(tinyMCE.activeEditor.dom.select('p:first'), 'lead');
+		setLead();
 	});
 
-	editor.on('keyup', function(e) {
-		tinyMCE.activeEditor.dom.removeClass(tinyMCE.activeEditor.dom.select('p'), 'lead');
-		tinyMCE.activeEditor.dom.addClass(tinyMCE.activeEditor.dom.select('p:first'), 'lead');
+	editor.on('change keyup', function(e) {
+		setLead();
 	});
+
+	function setLead() {
+
+		// Reset lead class and color in case it is not relevant any more.
+		var oldLead = tinyMCE.activeEditor.dom.select('p.lead');
+		if(oldLead.length) {
+			tinyMCE.activeEditor.dom.removeClass(oldLead, 'lead');
+			tinyMCE.activeEditor.dom.setStyle(oldLead, 'background-color', '');
+			tinyMCE.activeEditor.dom.setStyle(oldLead, 'color', '');
+		}
+
+		// Add .lead class to first paragraph
+		var lead = tinyMCE.activeEditor.dom.select('p:first');
+		tinyMCE.activeEditor.dom.addClass(lead, 'lead');
+
+		// Add red/white colors to lead paragraph if it is longer than 200 characters.
+		var leadLength = lead[0].innerHTML.length;
+		if (leadLength > 200) {
+			tinyMCE.activeEditor.dom.setStyle(lead, 'background-color', 'rgb(221, 61, 54)');
+			tinyMCE.activeEditor.dom.setStyle(lead, 'color', 'white');
+		}
+
+	}
 
 	editor.on('SaveContent', function(e) {
 		/**
