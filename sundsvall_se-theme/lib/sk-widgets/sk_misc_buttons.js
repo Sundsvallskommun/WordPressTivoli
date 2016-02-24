13d4d0afe4c33660fe1ffcebee0dd9a148aa9437
Johan Linder <johan@flatmate.se>
2016-02-24 14:08:03 +0100

    Youtube button improvements
    * Check url to see if it is a youtube or vimeo link. If not, display an
      error message.
    * Add red film icon.

--- sundsvall_se-theme/lib/sk-widgets/sk_misc_buttons.js
+++ sundsvall_se-theme/lib/sk-widgets/sk_misc_buttons.js
@@ -3,8 +3,8 @@
 	tinymce.create('tinymce.plugins.MiscButtons', {
 		init : function(ed, url) {
 			ed.addButton( 'youtube_button', {
-				title : 'Infoga Youtube-video',
-				image : templateDir + '/assets/images/admin/e-tjanst.png',
+				title : 'Infoga YoutTube-video',
+				image : templateDir + '/assets/images/admin/film.png',
 				onclick : function() {
 
 					ed.windowManager.open({
@@ -14,7 +14,7 @@
 						body: [
 							  {
                     type: 'container',
-										html: '<div>Wordpress har ett inbyggt stöd för att infoga videor. <br><br> Såhär gör du: </p> <ol><li>Gå till youtube och hitta videon du vill infoga. </li><li> Kopiera länken till videon (t.ex. http://www.youtube.com/watch?v=j1qBGQuOR8A) </li><li>Klistra in länken där du vill att videon ska visas så blir det automatiskt en video!</li></ol></div><br><br>',
+										html: '<div>Wordpress har ett inbyggt stöd för att infoga videor. <br><br> Såhär gör du: </p> <ol><li>Gå till youtube eller vimeo och hitta videon du vill infoga. </li><li> Kopiera länken till videon (t.ex. http://www.youtube.com/watch?v=j1qBGQuOR8A) </li><li>Klistra in länken där du vill att videon ska visas så blir det automatiskt en video!</li></ol></div><br><br>',
                 },
 							  {
                     type: 'textbox',
@@ -23,11 +23,18 @@
                 },
 						],
 						onSubmit: function(e) {
-							//isYoutube = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+
+							var isYoutube = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+							var isVimeo = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)/;
+
 							var videoUrl = this.toJSON().videoUrl;
-							//if(isYoutube.test(videoUrl)) {
+							if(isYoutube.test(videoUrl) || isVimeo.test(videoUrl)) {
 								ed.insertContent("<p>" + videoUrl + "</p>");
-							//}
+							} else if(videoUrl.length) {
+								ed.windowManager.alert("Länken verkar inte vara korrekt");
+							} else {
+
+							}
 						}
 					});
 
