af35577e77a5c8d9e9d7c54eaf4d448f51995489
Johan Linder <johan@flatmate.se>
2016-03-07 15:18:29 +0100

    Remove "at a glance" from wordpress panel

--- sundsvall_se-theme/lib/class-sk-init.php
+++ sundsvall_se-theme/lib/class-sk-init.php
@@ -32,6 +32,7 @@ class SK_Init {
 	function sk_remove_dashboard_widgets() {
 		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
 		remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
 	}
 
 	function image_setup() {
