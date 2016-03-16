6950eff164d4c810f23c07e1febb46daf0d75da0
Johan Linder <johan@flatmate.se>
2016-03-16 10:59:07 +0100

    Bug fixes for attachment photographer field

--- sundsvall_se-theme/lib/class-sk-attachments.php
+++ sundsvall_se-theme/lib/class-sk-attachments.php
@@ -19,9 +19,9 @@ class SK_Attachments {
 	 * Based on https://gist.github.com/kosinix/5493051
 	 */
 	function attachment_fields() {
-		add_filter( 'attachment_fields_to_edit', 'new_attachment_fields', 9, 2 );
-		add_filter( 'attachment_fields_to_save', 'update_attachment_meta', 4 );
-		add_action( 'wp_ajax_save-attachment-compat', 'media_extra_fields', 0, 1 );
+		add_filter( 'attachment_fields_to_edit', array(&$this, 'new_attachment_fields'), 9, 2 );
+		add_filter( 'attachment_fields_to_save', array(&$this, 'update_attachment_meta'), 4, 2);
+		add_action( 'wp_ajax_save-attachment-compat', array(&$this, 'media_extra_fields'), 0, 1 );
 	}
 
 	/**
@@ -41,18 +41,17 @@ class SK_Attachments {
 	/**
 	 * Update custom field on save
 	 */
-	function update_attachment_meta($attachment){
-		global $post;
-		update_post_meta($post->ID, 'media_photographer', $attachment['attachments'][$post->ID]['media_photographer']);
-		return $attachment;
+	function update_attachment_meta($post, $attachment){
+		update_post_meta($post['ID'], 'media_photographer', $attachment['media_photographer']);
+		return $post;
 	}
 
 	/**
 	 * Update custom field with ajax
 	 */
-	function mytheme_media_xtra_fields() {
+	function media_extra_fields() {
 		$post_id = $_POST['id'];
-		$meta = $_POST['attachments'][$post_id ]['media_photographer'];
+		$meta = $_POST['attachments'][$post_id]['media_photographer'];
 		update_post_meta($post_id , 'media_photographer', $meta);
 		clean_post_cache($post_id);
 	}
