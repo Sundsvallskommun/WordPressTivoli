cca338d471d0c3acf8ea4e48673a6b7e9d825a1c
Johan Linder <johan@flatmate.se>
2016-03-23 15:06:10 +0100

    Add calendar widget with some mock styling

--- sundsvall_se-theme/front-page.php
+++ sundsvall_se-theme/front-page.php
@@ -12,4 +12,12 @@
 
 </div>
 
+<div class="row">
+
+	<div class="col-md-12">
+		<?php get_template_part('partials/front-page/calendar'); ?>
+	</div>
+
+</div>
+
 <?php get_footer(); ?>
--- sundsvall_se-theme/partials/front-page/calendar.php
+++ sundsvall_se-theme/partials/front-page/calendar.php
@@ -1,6 +1,27 @@
-<section>
+<section class="front-page-section front-page-section__calendar">
 
-	<h2>Kalender</h2>
+
+<script type="text/javascript">
+	//<![CDATA[
+(function () {
+	var s = document.createElement('script'); s.type = 'text/javascript';s.async = true;
+	s.src = 'http://webapps.citybreak.com/597883702/sv/sv/widget/GenerateEventCalendarWidget?eventbaseurl=http%3A%2F%2Fguide.visitsundsvall.se%2Fsv%2Flink%2Fproduct%2F&eventlistingurl=http%3A%2F%2Fguide.visitsundsvall.se%2Fsv%2Fevenemang%2F&online3=true&defaultcss=true&images=false&css=http://resources.citybreak.com/citybreakweb/sundsvall/widget/css/style.css&truncate=true&blanktarget=true&geoId=3497'
+	var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+	})();
+	//]]>
+	</script>
+
+<div class="row">
+	<div class="col-md-5 col-md-push-7">
+	<h2><?php echo date('l'); ?><strong><?php echo date(' j F'); ?></strong></h2>
+	<h3>Detta händer i Sundsvall i dag</h3>
+		<div id="citybreak_event_calendar_widget"></div>
+	</div>
+</div>
+
+<style>
+
+</style>
 
 </section>
 
