553a1226d2173ebcc1760f2c5c725ae7ae9f1dea
Johan Linder <johan@flatmate.se>
2016-04-11 09:37:59 +0200

    Add single.php

---
+++ sundsvall_se-theme/single.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<div class="container-fluid">
+
+<div class="single-post__row">
+
+	<aside class="single-post__sidebar">
+
+		<a href="#post-content" class="focus-only"><?php _e('Hoppa över sidomeny', 'sundsvall_se'); ?></a>
+
+		<?php do_action('sk_page_helpmenu'); ?>
+
+	</aside>
+
+	<div class="single-post__content" id="post-content">
+
+		<?php do_action('sk_before_page_title'); ?>
+
+		<h1 class="single-post__title"><?php the_title(); ?></h1>
+
+		<?php do_action('sk_after_page_title'); ?>
+
+		<div class="single-post__date">
+			<span class="text-muted"><?php _e('Senast ändrad', 'sundsvall_se'); ?> :</span> <?php the_modified_date(); ?>
+		</div>
+
+		<?php do_action('sk_before_page_content'); ?>
+
+		<?php the_content(); ?>
+
+		<?php do_action('sk_after_page_content'); ?>
+
+	</div>
+
+</div> <?php //.row ?>
+
+</div> <?php //.container-fluid ?>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
