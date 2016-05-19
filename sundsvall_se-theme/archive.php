5b8d7e2e5e13d333a232c55e06d71767f5a667ef
Johan Linder <johan@flatmate.se>
2016-05-19 09:31:49 +0200

    Add correct heading class to archive page

--- sundsvall_se-theme/archive.php
+++ sundsvall_se-theme/archive.php
@@ -18,7 +18,7 @@ get_header();
 
 	<div class="archive__content">
 
-		<h1 class="archive-title"><?php the_archive_title(); ?></h1>
+		<h1 class="archive__title"><?php the_archive_title(); ?></h1>
 
 		<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
 
