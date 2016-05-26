b8a942b07247658a298151624e4aeddd28f02269
Johan Linder <johan@flatmate.se>
2016-05-26 11:18:27 +0200

    Correct spelling error of finnish link

--- sundsvall_se-theme/partials/site-navbar.php
+++ sundsvall_se-theme/partials/site-navbar.php
@@ -3,7 +3,7 @@
 		<div class="nav navbar-nav pull-xs-right">
 			<a class="nav-item nav-link btn btn-purple btn-sm" href="https://e-tjanster.sundsvall.se/">Våra e-tjänster</a>
 
-			<a class="nav-item nav-link btn btn-primary btn-sm" lang="fi" href="<?php echo get_permalink(get_page_by_title( 'Soumeksi')->ID); ?>">Soumeksi</a>
+			<a class="nav-item nav-link btn btn-primary btn-sm" lang="fi" href="<?php echo get_permalink(get_page_by_title( 'Suomeksi')->ID); ?>">Suomeksi</a>
 
 			<?php get_template_part('./partials/google-translate'); ?>
 
