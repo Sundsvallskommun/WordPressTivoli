e26e289901a79a73914d0a25ef4740e56ae01bb7
Johan Linder <johan@flatmate.se>
2016-05-31 09:23:34 +0200

    Move mobile menu button to right, add label and icon

--- sundsvall_se-theme/header.php
+++ sundsvall_se-theme/header.php
@@ -2,15 +2,15 @@
 
 <nav class="hidden-md-up navbar navbar-light navbar-full navbar-mobile navbar-fixed-top">
 
-	<a class="navbar-toggler btn" data-toggle="offcanvas-left" href="#mainNavigation"> &#9776; </a>
+	<a class="navbar-toggler btn" data-toggle="search"
+	href="#searchContainer"><span class="iconwrapper"><?php the_icon('search'); ?></span></a>
 
 	<div class="navbar-center">
 			<?php get_template_part('partials/site-logo'); ?>
 	</div>
 
+	<a class="navbar-toggler pull-xs-right btn" data-toggle="offcanvas-left" aria-hidden="true" href="#mainNavigation"><span>Meny</span><span class="iconwrapper"><?php the_icon('menu'); ?></span></a>
 
-		<a class="navbar-toggler pull-xs-right btn" data-toggle="search"
-						href="#searchContainer"><?php the_icon('search'); ?></a>
 </nav>
 
 <header class="site-header">
