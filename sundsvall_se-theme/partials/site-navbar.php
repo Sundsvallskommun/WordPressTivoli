beb16550a8b556b8d2fea66a6aa4518258909755
Johan Linder <johan@flatmate.se>
2016-06-02 11:00:58 +0200

    Add login dropdown to navbar

--- sundsvall_se-theme/partials/site-navbar.php
+++ sundsvall_se-theme/partials/site-navbar.php
@@ -1,7 +1,21 @@
 <nav class="navbar navbar-full hidden-sm-down">
 	<div class="container-fluid">
 		<div class="nav navbar-nav pull-xs-right">
-			<a class="nav-item nav-link btn btn-purple btn-sm" href="https://e-tjanster.sundsvall.se/">Våra e-tjänster</a>
+
+			<div class="nav-item dropdown">
+
+				<button class="btn btn-purple btn-sm nav-link dropdown-toggle" lang="en" type="button" id="languageMenuButton" aria-haspopup="true" aria-expanded="false">
+					Logga in
+				</button>
+
+				<div class="dropdown-menu" aria-labelledby="languageMenuButton">
+					<a class="dropdown-item" href="#">Elev</a>
+					<a class="dropdown-item" href="#">Medborgare</a>
+					<a class="dropdown-item" href="#">Medarbetare</a>
+				</div>
+			</div>
+
+			<?php get_template_part('./partials/google-translate'); ?>
 
 			<?php
 				$finnish_page = get_page_by_title( 'Suomeksi');
@@ -9,10 +23,6 @@
 			?>
 			<a class="nav-item nav-link btn btn-primary btn-sm" lang="fi" href="<?php echo get_permalink($finnish_page->ID); ?>">Suomeksi</a>
 			<?php endif; ?>
-
-			<?php get_template_part('./partials/google-translate'); ?>
-
-			<a class="nav-item nav-link btn btn-primary btn-sm" href="#">Lyssna</a>
 
 		</div>
 	</div>
