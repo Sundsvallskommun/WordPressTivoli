41f6d89ccd6f6dc1dc8c9e719686d5b0b913a5b3
Johan Linder <johan@flatmate.se>
2016-06-07 10:43:01 +0200

    Load google translate script in footer

--- sundsvall_se-theme/partials/google-translate.php
+++ sundsvall_se-theme/partials/google-translate.php
@@ -8,13 +8,21 @@
 		<h6 class="dropdown-header" lang="en">Translate</h6>
 		<p class="dropdown-item" lang="en" style="white-space: normal;"><small>Use Google to translate the web site. We take no responsibility for the accuracy of the translation.</small></p>
 
-		<div class="dropdown-item" id="google_translate_element"></div><script type="text/javascript">
-		function googleTranslateElementInit() {
-			new google.translate.TranslateElement({pageLanguage: 'sv', autoDisplay: false, multilanguagePage: false}, 'google_translate_element');
-		}
-		</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+		<div class="dropdown-item" id="google_translate_element"></div>	</div>
 
-	</div>
+<?php 
+// Load translate script/styles in footer to prevent it from blocking rendering
+add_action('wp_footer', function() {
+?>
+<script type="text/javascript">
+	function googleTranslateElementInit() {
+		new google.translate.TranslateElement({pageLanguage: 'sv', autoDisplay: false, multilanguagePage: false}, 'google_translate_element');
+	}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<?php
+});
+?>
 
 </div>
 
