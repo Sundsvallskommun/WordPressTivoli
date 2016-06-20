280f22f8f4be6cccae31f5580eb5c1213ce546e0
Johan Linder <johan@flatmate.se>
2016-06-20 09:28:29 +0200

    Only show upload button in document list when user can upload files

--- sundsvall_se-theme/lib/sk-documents/class-sk-documents.php
+++ sundsvall_se-theme/lib/sk-documents/class-sk-documents.php
@@ -212,7 +212,7 @@ class SK_Documents {
 			$links .= sprintf('<a class="list-group-item" href="%s"><span class="label label-default label-pill pull-xs-right">%s, %s</span> %s</a>', $doc['url'], $doc['filetype'], $doc['size'], $doc['title']);
 		}
 
-		if (is_user_logged_in() ) {
+		if ( is_user_logged_in() && current_user_can( 'upload_files' ) ) {
 			$upload_url = get_site_url().'/wp-admin/upload.php?mode=grid&rml_folder='.$id;
 			$links .= '<a class="list-group-item text-xs-center font-weight-bold" href="'.$upload_url.'">Ladda upp filer</a>';
 		}
