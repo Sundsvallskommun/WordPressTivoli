82c215e5e0f63c6dd141a13f382dfbce84483f47
Johan Linder <johan@fmca.se>
2016-08-15 14:31:11 +0200

    Set expired posts status to draft on hourly schedule

--- sundsvall_se-theme/lib/class-sk-expiration.php
+++ sundsvall_se-theme/lib/class-sk-expiration.php
@@ -12,13 +12,22 @@ class SK_Expiration {
 		add_action('admin_enqueue_scripts', array( &$this, 'datepicker_scripts' ) );
 		add_action('add_meta_boxes', array( &$this, 'add_expiration_metabox' ) );
 		add_action('save_post', array( &$this, 'save_expiration_meta' ) );
+
+		add_action( 'sk_unpublish_expired', array( &$this, 'unpublish_expired_content' ) );
+		add_action('init', array( &$this, 'unpublish_expired_content_cron' ) );
 	}
 
+	/**
+	 * Enqueue scripts and styles for jquery ui datepicker
+	 */
 	public function datepicker_scripts() {
 		wp_enqueue_script( 'jquery-ui-datepicker' );
 		wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
 	}
 
+	/**
+	 * Add metabox to posts where we will add datepicker input
+	 */
 	public function add_expiration_metabox() {
 		add_meta_box(
 			'sk_expiration_metabox',
@@ -30,6 +39,9 @@ class SK_Expiration {
 		);
 	}
 
+	/**
+	 * Add datepicker form with custom validation.
+	 */
 	public function sk_expiration_metabox_callback($post) { 
 
 		wp_nonce_field( 'sk_expiry_date_nonce', 'sk_nonce' ); ?>
@@ -96,6 +108,9 @@ class SK_Expiration {
 
 	<?php }
 
+	/**
+	 * Save expiration date on post save
+	 */
 	public function save_expiration_meta( $post_id ) {
 
 		if( !isset( $_POST['sk_nonce'] ) ||
@@ -104,7 +119,7 @@ class SK_Expiration {
 			) ) 
 		return;
 
-		if( !current_user_can( 'edit_post', $post->ID ) ) {
+		if( !current_user_can( 'edit_post', $post_id ) ) {
 			return;
 		}
 
@@ -112,6 +127,41 @@ class SK_Expiration {
 			$expiry_date = ( $_POST['sk_expiry_date'] );
 
 			update_post_meta( $post_id, self::META_NAME, $expiry_date );
+		}
+
+	}
+
+	public function unpublish_expired_content_cron() {
+
+		if (! wp_next_scheduled ( 'sk_unpublish_expired' )) {
+			wp_schedule_event(time(), 'hourly', 'sk_unpublish_expired');
+		}
+
+	}
+
+	public function unpublish_expired_content() {
+
+		$today = date('Y-m-d');
+
+		$query_args = array(
+				'meta_key' => self::META_NAME,
+				'meta_value' => $today,
+				'meta_compare' => '<=',
+				'meta_type' => 'DATE'
+		);
+
+		$query = new WP_Query( $query_args );
+
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) {
+				$query->the_post();
+
+				wp_update_post( array(
+					'ID'          => get_the_id(),
+					'post_status' => 'draft'
+				));
+
+			}
 		}
 
 	}
