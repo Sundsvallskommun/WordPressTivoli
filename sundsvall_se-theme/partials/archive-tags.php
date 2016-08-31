d8145b1172c2f1fc46df5c4fe1d03061c195543c
Johan Linder <johan@fmca.se>
2016-08-31 15:59:43 +0200

    Use categories instead of tags to allow user to sort from

--- sundsvall_se-theme/partials/archive-tags.php
+++ sundsvall_se-theme/partials/archive-tags.php
@@ -4,18 +4,18 @@
 $page_for_posts = get_option( 'page_for_posts' );
 
 if (is_tag() || is_home()) {
-	$tags = get_field( 'archive_displayed_tags', $page_for_posts );
+	$categories = get_field( 'archive_displayed_categories', $page_for_posts );
 }
 
 if (is_advanced_template_child()) {
-	$tags = get_field( 'archive_displayed_tags', get_queried_object_id());
+	$categories = get_field( 'archive_displayed_categories', get_queried_object_id());
 }
 
-if($tags) {
-	echo'<h2>'; _e( 'Sortera efter taggar:', 'sundsvall_se' ); echo '</h2>';
+if($categories) {
+	echo'<h2>'; _e( 'Sortera efter kategorier:', 'sundsvall_se' ); echo '</h2>';
 	echo '<div class="archive__tags post-tags">';
-	foreach( $tags as $tag ) {
-		echo '<a rel="tag" href="' . get_tag_link( $tag->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $tag->name ) . '" ' . '>' . $tag->name.'</a> ';
+	foreach( $categories as $category ) {
+		echo '<a rel="tag" href="' . get_tag_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->name.'</a> ';
 	}
 	echo '</div>';
 }
