e7e82bd2f699d73f0d1e5844cca6407e0be50b89
Johan Linder <johan@fmca.se>
2016-10-06 15:29:01 +0200

    Remove parking space status shortcode

--- sundsvall_se-theme/functions.php
+++ sundsvall_se-theme/functions.php
@@ -318,15 +318,6 @@ require_once locate_template( 'lib/sk-vacancy/class-sk-vacancy.php' );
 $sk_vacancy = SK_Vacancy::get_instance();
 
 /**
- * SK_Parking
- * ==========
- *
- * Functionality for showing list and single vacancies
- */
-require_once locate_template( 'lib/sk-parking/class-sk-parking.php' );
-$sk_parking = SK_Parking::get_instance();
-
-/**
  * SK_Admincolumns
  * ===============
  *
