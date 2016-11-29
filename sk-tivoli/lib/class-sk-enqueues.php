1bdf64f11363a5cdfebfb9d1c87559f5533cb116
Johan Linder <johan@fmca.se>
2016-11-29 14:04:32 +0100

    Remove all customizations to how scripts are loaded

--- sk-tivoli/lib/class-sk-enqueues.php
+++ sk-tivoli/lib/class-sk-enqueues.php
@@ -19,9 +19,6 @@ class SK_Enqueues {
 		add_action( 'wp_enqueue_scripts', array(&$this, 'sk_enqueue_styles') );
 		add_action( 'wp_enqueue_scripts', array(&$this, 'sk_enqueue_scripts'), 10 );
 
-		add_action( 'wp_enqueue_scripts', array(&$this, 'scripts_to_footer') );
-		add_action( 'init', array(&$this, 'change_jquery_version') );
-
 		add_action( 'admin_init', array(&$this, 'sk_add_editor_styles') );
 
 	}
@@ -63,56 +60,5 @@ class SK_Enqueues {
 		add_editor_style( $this->sk_font_url );
 		add_editor_style( './assets/css/editor-styles.css' );
 	}
-
-	/**
-	 * Enqueue all scripts in footer instead of head to prevent them from
-	 * blocking rendering above the fold.
-	 */
-	function scripts_to_footer() {
-
-		remove_action('wp_head', 'wp_print_scripts');
-		remove_action('wp_head', 'wp_print_head_scripts', 9);
-		remove_action('wp_head', 'wp_enqueue_scripts', 1);
-
-		add_action('wp_footer', 'wp_print_scripts', 5);
-		add_action('wp_footer', 'wp_enqueue_scripts', 5);
-		add_action('wp_footer', 'wp_print_head_scripts', 5);
-	}
-
-
-	/**
-	 * Gravity form with ajax enabled inlines script that depends on jQuery,
-	 * which break when we move scripts to footer, because of that we will add it
-	 * to the header instead.
-	 *
-	 * We also load from google CDN with local fallback.
-	 */
-	function change_jquery_version() {
-
-		if( $GLOBALS['pagenow'] === 'wp-login.php' ) return;
-
-		if ( !is_admin() ) {
-			// Deregister and dequeue the WP included jQuery.
-			wp_deregister_script('jquery');
-			wp_dequeue_script('jquery');
-
-			// Register and enqueue our own jQuery file.
-			// NOTE: This file is empty since we're actually loading jQuery from
-			// Googles CDN in the header.
-			// 
-			// The reason we enqueue an empty file is because some scripts depends on
-			// jQuery being registered and enqueued.
-			wp_register_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery.js', false, '1.8.7' );
-			wp_enqueue_script( 'jquery' );
-
-			add_action('wp_head', function() {
-				// Google CDN
-				echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>';
-				// Local fallback
-				echo '<script>if (!window.jQuery) { document.write(\'<script src="'. get_stylesheet_directory_uri() .'/assets/js/source/vendor/jquery-2.2.4.min.js'.'"><\/script>\'); }</script>';
-			}, 10);
-		}
-	}
-
 
 }
